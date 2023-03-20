5274755a7ff4cd974fabc684503aed002b5b7307
Walter Fabri <walterfabri18@gmail.com>
2023-03-19 23:21:29 -0300

    create home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,3 +106,14 @@ function ladoAleatorio() {
             return 'ladoB';
     }
 }
+
+function iniciarJogo() {
+    let nivel = document.getElementById('nivel').value;
+
+    if (nivel === '') {
+        alert('Selecione um nível para iniciar o jogo!');
+        return false;
+    }
+
+    
+}
